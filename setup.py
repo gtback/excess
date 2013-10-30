94266231c6897faaba3a5b673c238a240c716c63
Greg Back <gback@mitre.org>
2013-10-30 16:25:09 -0400

    Add Travis-CI config

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2013, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
+import sys
+
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 import xs
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 install_requires = []
 
@@ -14,8 +30,9 @@ setup(
     author_email="gback@mitre.org",
     description="Make XML Schemas feel more like Python",
     packages=find_packages(),
-    install_requires=install_requires,
-    test_requires=['pytest'],
+    install_requires=[],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
     classifiers=[
         "Programming Language :: Python",
         "Development Status :: 2 - Pre-Alpha",
